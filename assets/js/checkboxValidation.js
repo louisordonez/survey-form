cf4751cb870179a1fe1d77c535fe70f57627f696
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-05-05 20:00:38 +0800

    Fix checkbox validation

--- assets/js/checkboxValidation.js
+++ assets/js/checkboxValidation.js
@@ -1,6 +1,11 @@
 function checkboxValidationFunction() {
   let checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
   if (checkboxes.length == 0) {
+    const element = document.querySelector("form");
+    element.addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log("Form submission cancelled.");
+    });
     alert("At least one checkbox must be selected.");
   }
 }
